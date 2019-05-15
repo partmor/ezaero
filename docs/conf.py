b1e8e803ff80995f2a53c219485c55b0730d64f5
partmor <part.morales@gmail.com>
2019-05-15 20:40:23 +0200

    add intersphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -175,4 +175,9 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'numpy': ('https://docs.scipy.org/doc/numpy/', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy/reference', None),
+    'matplotlib': ('http://matplotlib.org', None)
+}
