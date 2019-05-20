8fcbb3a4856af47e9cc942e9462f4a567b38354c
partmor <part.morales@gmail.com>
2019-05-20 23:27:15 +0200

    add test comparing to slender wing theory
    and remove redundant test for profile theory

--- tests/test_steady.py
+++ tests/test_steady.py
@@ -23,18 +23,18 @@ def test_cl_for_infinite_wing(alpha):
     assert sim.cl_wing == pytest.approx(2 * np.pi * alpha, rel=5e-3)
 
 
-def test_cl_slope_for_infinite_wing():
-    cls = []
-    alphas = [1 * np.pi / 180, 2 * np.pi / 180]
-    for alpha in alphas:
-        flcond = vlm_steady.FlightConditions(ui=50.0, alpha=alpha, rho=1.0)
-        sim = vlm_steady.run_simulation(
-            wing=INFINITE_WING['wing'],
-            mesh=INFINITE_WING['mesh'],
-            flcond=flcond
-        )
-        cls.append(sim.cl_wing)
+def test_cl_slope_vs_aspect_ratio_for_slender_wing():
+    bps = [0.25, 0.5]
+    mesh = vlm_steady.MeshParams(8, 30)
+    alpha = np.pi / 180
+    flcond = vlm_steady.FlightConditions(ui=100.0, alpha=alpha, rho=1.0)
+    clas = []
+    for bp in bps:
+        wing = vlm_steady.WingParams(cr=1.0, ct=1.0, bp=bp, theta=0.0,
+                                     delta=0.0)
+        sim = vlm_steady.run_simulation(wing=wing, mesh=mesh, flcond=flcond)
+        clas.append(sim.cl_wing / alpha)
 
-    slope = (cls[1] - cls[0]) / (alphas[1] - alphas[0])
+    slope = (clas[1] - clas[0]) / (bps[1] - bps[0])
 
-    assert slope == pytest.approx(2 * np.pi, abs=1e-2)
+    assert slope == pytest.approx(np.pi / 2, abs=1e-2)
