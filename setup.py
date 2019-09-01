67be220dc7a4942539cd2bb95a96c7d8ceb6f89c
partmor <part.morales@gmail.com>
2019-09-01 18:30:07 +0000

    add info source on configuration using setup.cfg

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 
-# https://packaging.python.org/guides/single-sourcing-package-version/
-# http://blog.ionelmc.ro/2014/05/25/python-packaging/
+# - https://packaging.python.org/guides/single-sourcing-package-version/
+# - http://blog.ionelmc.ro/2014/05/25/python-packaging/
+# - https://setuptools.readthedocs.io/en/latest/setuptools.html
+#       section: configuring-setup-using-setup-cfg-files
 
 setup()
