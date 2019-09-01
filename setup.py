aaae4be68bcad07320d4a85ecc336c437d13d641
partmor <part.morales@gmail.com>
2019-09-01 18:05:22 +0000

    Revert "move version number back to setup.py"
    This reverts commit edd83fa6a1fab90cdea43b2750943b9be66c15c2.

--- setup.py
+++ setup.py
@@ -2,13 +2,5 @@ from setuptools import setup
 
 # https://packaging.python.org/guides/single-sourcing-package-version/
 # http://blog.ionelmc.ro/2014/05/25/python-packaging/
-# https://setuptools.readthedocs.io/en/latest/setuptools.html
 
-# version not in setup.cfg since read from file is a recent setuptools
-# feature, and is troublesome for users with older setuptool versions
-with open('VERSION') as version_file:
-    VERSION = version_file.read().strip()
-
-setup(
-    version=VERSION
-)
+setup()
